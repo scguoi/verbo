ebc664ca68d34d1e1d6a8dcfe8f6d25933aafd39
scguoi <guosongchu@126.com>
2026-04-11 10:57:05 +0800

    feat: add text output (simulate + clipboard) and system tray

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,26 @@
+mod config;
+mod hotkey;
+mod output;
+mod tray;
+
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_clipboard_manager::init())
+        .setup(|app| {
+            tray::create_tray(app.handle()).map_err(|e| Box::<dyn std::error::Error>::from(e))?;
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            config::read_config,
+            config::write_config,
+            config::get_config_path,
+            hotkey::register_hotkeys,
+            hotkey::unregister_all_hotkeys,
+            output::simulate_input,
+            output::copy_to_clipboard,
+            output::check_accessibility_permission,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
